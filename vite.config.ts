a0321103008559a566a6a117fe3431573c23dd1c
Wayne Van Son <waynevanson@gmail.com>
2025-02-08 21:33:30 +1100

    omg fix vitest

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import solid from "vite-plugin-solid"
 
 export default defineConfig({
   plugins: [solid()],
-  resolve: { conditions: ["development", "browser"] },
-  test: {
-    server: { deps: { inline: true } },
-  },
 })
