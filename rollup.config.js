27ef39313d0392cab69fbf74adc36fbca5f40254
Wayne Van Son <waynevanson@gmail.com>
2025-02-09 10:30:22 +1100

    build: use vite as config

--- rollup.config.js
+++
@@ -1,7 +0,0 @@
-//@ts-check
-import withSolid from "rollup-preset-solid"
-import minify from "@rollup/plugin-terser"
-
-export default withSolid({
-  input: "src/index.tsx",
-})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite"
 import solid from "vite-plugin-solid"
+import withSolid from "rollup-preset-solid"
+
+const rollupOptions = withSolid({
+  input: "src/index.tsx",
+})
+
+rollupOptions.output.forEach((output) => {
+  delete output.sourcemap
+  output.entryFileNames = "[name].js"
+  output.chunkFileNames = "[name].js"
+})
 
 export default defineConfig({
   plugins: [solid()],
+  build: {
+    rollupOptions,
+    sourcemap: true,
+  },
 })
